77ddee9301cbf3767aefc4f2b18ac04c71fe7850
Timofey <me@tx0.su>
2025-10-17 21:02:55 +0500

    [u] implement FindEntries function

--- entry/parser.go
+++ entry/parser.go
@@ -2,6 +2,7 @@ package entry
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -58,6 +59,55 @@ func ParseEntry(entry_file *os.File) (*BootEntry, error) {
 	}
 
 	return entry, nil
+}
+
+func FindEntries(dir string) ([]*BootEntry, error) {
+	var entries []*BootEntry
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return nil, fmt.Errorf("directory %s does not exist", dir)
+	}
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if isEntryFile(info.Name()) {
+			file, err := os.Open(path)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: failed to open %s: %v\n", path, err)
+				return nil
+			}
+			defer file.Close()
+			entry, err := ParseEntry(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Warning: failed to parse %s: %v\n", path, err)
+				return nil
+			}
+			entries = append(entries, entry)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error scanning directory %s: %v", dir, err)
+	}
+
+	return entries, nil
+}
+
+func isEntryFile(filename string) bool {
+	if strings.HasSuffix(filename, ".conf") {
+		return true
+	}
+
+	return false
 }
 
 func (e *BootEntry) CleanupEntry() {
